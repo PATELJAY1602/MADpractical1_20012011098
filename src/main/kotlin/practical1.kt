a7f3147a99c2ed470c362bbe53962264a8ae0b7a
PATELJAY1602 <pateljayj443@gmail.com>
2022-08-08 22:41:12 +0530

    run

---
+++ src/main/kotlin/practical1.kt
@@ -0,0 +1,22 @@
+fun main(){
+    var i:Int = 100
+    var f:Float = 12.23f
+    var c:Char ='J'
+    var s:String = "JAY"
+    var b:Boolean = false
+    var d:Double = 96.36
+    var l:Long = 20012011098
+    var S:Short = -2
+    var B:Byte = 127
+
+    println("Integer Value : $i")
+    println("Float Value : $f")
+    println("Character Value : $c")
+    println("String Value : $s")
+    println("Boolean Value : $b")
+    println("Double Value : $d")
+    println("Long Value : $l")
+    println("Short Value :$S")
+    println("Byte Value : $B")
+
+}
