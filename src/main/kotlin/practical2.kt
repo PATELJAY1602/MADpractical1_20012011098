a53ba31460371c6fb5eba6474948079626fb55ee
PATELJAY1602 <pateljayj443@gmail.com>
2022-08-23 18:39:20 +0530

    run

---
+++ src/main/kotlin/practical2.kt
@@ -0,0 +1,14 @@
+fun main(){
+    var i: Int = 98
+    println("Integer Value : $i")
+    var a = i.toDouble()
+    println("Double Value (From Integer) : $a")
+    var s: String = "98"
+    println("String Value : $s")
+    var b = s.toInt()
+    println("Integer Value1 : $b")
+    var d: String = "10"
+    println("String Value : $d")
+    var c = d.toDouble()
+    println("Double Value (From String) : $c")
+}
